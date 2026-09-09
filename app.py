0b83d8dc0600f2e524e05c8a02b3aabd168a4e9f
maty725 <maty.mbaye@student.junia.com>
2026-09-09 12:46:41 +0200

    amelioration documentation et structure du depot

--- app.py
+++ app.py
@@ -629,7 +629,6 @@ with st.sidebar:
     st.markdown("""
     <div style='color:rgba(255,255,255,0.5); font-size:0.7rem; text-align:center; margin-top:1rem;'>
         Mémoire fin d'études 2025–2026<br>
-        [Nom du Candidat]<br>
         Hôpital Principal de Dakar
     </div>
     """, unsafe_allow_html=True)
